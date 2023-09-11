6f6d34bdbb521c197b5ed27ea6996f1ee707a693
maouia <maouiabend@gmail.com>
2023-09-11 11:57:59 +0100

    simplify code

--- op_functions_2.c
+++ op_functions_2.c
@@ -9,8 +9,7 @@ void pint(stack_t **stack, unsigned int line)
 {
 	if (*stack == NULL || stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line);
-		exit(EXIT_FAILURE);
+        error_exit_d("L%u: can't pint, stack empty\n", line, EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
