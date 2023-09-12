09dd56c02f340a72b0dc9e5932413b99d2e6caa5
maouia <maouiabend@gmail.com>
2023-09-12 17:43:00 +0100

    add if condition

--- monty.c
+++ monty.c
@@ -9,27 +9,27 @@ char *arg;
 
 int main(int argc, char **argv)
 {
-    FILE *fd;
-    char *buffer = NULL;
-    char *op;
-    size_t len = 0;
-    ssize_t nread = 1;
+	FILE *fd;
+	char *buffer = NULL;
+	char *op;
+	size_t len = 0;
+	ssize_t nread = 1;
 	unsigned int nline = 1;
 	stack_t *top = NULL;
-    void (*f)(stack_t **, unsigned int);
+	void (*f)(stack_t **, unsigned int);
 
 	if (argc != 2)
 		error_exit_s("USAGE: monty file\n", NULL, EXIT_FAILURE);
-	
+
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
 		error_exit_s("Error: Can't open file %s\n", argv[1], EXIT_FAILURE);
-	
-	
-	while ((nread = getline(&buffer, &len , fd)) != -1)
+
+
+	while ((nread = getline(&buffer, &len, fd)) != -1)
 	{
 		op = strtok(buffer, DELIM);
-		if(op == NULL)
+		if (op == NULL)
 		{
 			nline++;
 			continue;
@@ -42,11 +42,20 @@ int main(int argc, char **argv)
 			free_dlistint(top);
 			error_exit_d("L%u: usage: push integer\n", nline, EXIT_FAILURE);
 		}
+
 		f = get_op_func(op);
-		f(&top ,nline);
+		if (!f)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", nline, op);
+			fclose(fd);
+			free(buffer);
+			free_dlistint(top);
+			exit(EXIT_FAILURE);
+		}
+		f(&top, nline);
 		nline++;
-	} 
-	
+	}
+
 	fclose(fd);
 	free(buffer);
 	free_dlistint(top);
