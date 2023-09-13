a617532a23f70baf72624852e40480d78e56462c
hakimbouharb <wisedaniel45@gmail.com>
2023-09-13 04:22:44 -0700

    monty.c

--- monty.c
+++ monty.c
@@ -5,18 +5,10 @@
  * @argv: argument variables
  * Return: success 0   on error 1
 */
-char *arg;
 
 int main(int argc, char **argv)
 {
 	FILE *fd;
-	char *buffer = NULL;
-	char *op;
-	size_t len = 0;
-	ssize_t nread = 1;
-	unsigned int nline = 1;
-	stack_t *top = NULL;
-	void (*f)(stack_t **, unsigned int);
 
 	if (argc != 2)
 		error_exit_s("USAGE: monty file\n", NULL, EXIT_FAILURE);
@@ -25,39 +17,7 @@ int main(int argc, char **argv)
 	if (fd == NULL)
 		error_exit_s("Error: Can't open file %s\n", argv[1], EXIT_FAILURE);
 
+	read_and_execute_instructions(fd);
 
-	while ((nread = getline(&buffer, &len, fd)) != -1)
-	{
-		op = strtok(buffer, DELIM);
-		if (op == NULL)
-		{
-			nline++;
-			continue;
-		}
-		arg = strtok (NULL, DELIM);
-		if (!strcmp(op, "push") && !int_valid(arg))
-		{
-			fclose(fd);
-			free(buffer);
-			free_dlistint(top);
-			error_exit_d("L%u: usage: push integer\n", nline, EXIT_FAILURE);
-		}
-
-		f = get_op_func(op);
-		if (!f)
-		{
-			fprintf(stderr, "L%u: unknown instruction %s\n", nline, op);
-			fclose(fd);
-			free(buffer);
-			free_dlistint(top);
-			exit(EXIT_FAILURE);
-		}
-		f(&top, nline);
-		nline++;
-	}
-
-	fclose(fd);
-	free(buffer);
-	free_dlistint(top);
-	return (0);
+	return (EXIT_SUCCESS);
 }
